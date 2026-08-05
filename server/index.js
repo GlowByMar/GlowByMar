7c5e6b6b4f5e6bd9a97d3d133d3cedf95873edad
GlowByMar <elacampos1986@gmail.com>
2026-08-04 21:08:11 -0500

    Simplificar conexion mongoose

--- server/index.js
+++ server/index.js
@@ -53,11 +53,8 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// // CONEXIÓN A MONGODB ATLAS ULTRA ESTABLE
-mongoose.connect(process.env.MONGO_URL, {
-    serverSelectionTimeoutMS: 45000, // Espera hasta 45 segundos antes de rendirse
-    socketTimeoutMS: 45000,
-})
+// CONEXIÓN A MONGODB ATLAS
+mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('¡Conectado permanentemente a MongoDB Atlas!'))
 .catch(err => console.error('Error crítico al conectar a MongoDB:', err));
 
